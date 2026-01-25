b60182daaa568d84f34adf47cd8fc2979e95e13f
DETROMIN27 <94011680+DETROMIN27@users.noreply.github.com>
2026-01-25 16:23:51 +0200

    update

--- agario.py
+++ agario.py
@@ -1,14 +1,14 @@
 # Імпортуємо бібліотеку для створення ігор
-import
+import pygame
 
 # Імпортуємо функцію для генерації випадкових чисел
-from 
+from random import randint
 
 # Імпортуємо всі класи та функції з файлу my_class.py
-from 
+from my_class import *
 
 # Ініціалізуємо всі модулі Pygame
-pygame.
+pygame.init()
 
 # Задаємо ширину і висоту вікна гри
 WIDTH, HEIGHT = 500, 500
@@ -21,38 +21,69 @@ GREEN = (0, 200, 0)
 
 
 # Створюємо вікно гри з заданими розмірами
-window = 
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 # Створюємо об'єкт для контролю частоти кадрів (FPS)
-clock = 
+clock = pygame.time.Clock()
 
 # Змінна для збереження зображення фону (поки що порожня)
-fon = 
+fon = pygame.image.load("fon.png")
+fon = pygame.transform.scale(fon, (WIDTH, HEIGHT))
+#Гравець - eat
+player = Player(x=WIDTH//2,radius=20,speed=5,color=GREEN)
+
+foods = [Food()for i in range(300)]
 
 
 # Змінна для керування роботою гри
-run = 
+run = True
+
+fon_x,fon_y = 0,0 
+
+
 
 
 # Головний цикл гри, працює поки run == True
-while 
+while run:
+
 
     # Зафарбовуємо екран чорним кольором
-    window.
+    window.fill(BLACK)
+
 
     # Малюємо фон у верхньому лівому куті (помилка, якщо fon == None)
-    window.
+    window.blit(fon,(fon_x,fon_y)) 
+    window.blit(fon,(fon_x-WIDTH,fon_y)) 
+    window.blit(fon,(fon_x,fon_y-HEIGHT)) 
+    window.blit(fon,(fon_x-WIDTH,fon_y-HEIGHT))
+    player.draw(window)
+    player.move()
+
+    fon_x += player.move_x
+    fon_y += player.move_y
+    fon_x %=WIDTH
+    fon_y %=HEIGHT
+
+#cerepawka == eat 
+
+    for food in foods:
+        food.draw(window)
+        food.update(player)
+        if food.eat_me(player):
+             foods.remove(food)
+
 
     # Перебираємо всі події (натискання клавіш, закриття вікна тощо)
-    for 
+    for event in pygame.event.get():
+        
 
         # Перевіряємо, чи користувач закрив вікно
-        if 
+        if event.type == pygame.QUIT:
 
-            # Зупиняємо гру
-            run 
+                # Зупиняємо гру
+                run = False
 
     # Оновлюємо зображення на екрані
-    pygame.
+    pygame.display.flip()
 
     # Обмежуємо швидкість гри до 60 кадрів на секунду
-    clock.
+    clock.tick(60)
