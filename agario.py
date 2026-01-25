5e6cec17ce163f21d1b38cc975902438ca732779
a6086394-droid <a6086394@gmail.com>
2026-01-25 15:49:39 +0200

    jj

--- agario.py
+++ agario.py
@@ -1,14 +1,14 @@
 # Імпортуємо бібліотеку для створення ігор
-import
+import pygame
 
 # Імпортуємо функцію для генерації випадкових чисел
-from 
+from random import randint
 
 # Імпортуємо всі класи та функції з файлу my_class.py
-from 
+from my_class import*
 
 # Ініціалізуємо всі модулі Pygame
-pygame.
+pygame.init()
 
 # Задаємо ширину і висоту вікна гри
 WIDTH, HEIGHT = 500, 500
@@ -21,38 +21,55 @@ GREEN = (0, 200, 0)
 
 
 # Створюємо вікно гри з заданими розмірами
-window = 
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 # Створюємо об'єкт для контролю частоти кадрів (FPS)
-clock = 
+clock = pygame.time.Clock()
 
 # Змінна для збереження зображення фону (поки що порожня)
-fon = 
+fon = pygame.image.load("fon.png")
+fon = pygame.transform.scale(fon, (WIDTH, HEIGHT))
+
+# гравець
+player = Player(x=WHITE//2, y=HEIGHT//2, radius=20, speed=5,
+                color=GREEN)
 
 
 # Змінна для керування роботою гри
-run = 
+run = True
+
+fon_x,fon_y=0,0
 
 
 # Головний цикл гри, працює поки run == True
-while 
+while run:
 
     # Зафарбовуємо екран чорним кольором
-    window.
+    window.fill(BLACK)
 
     # Малюємо фон у верхньому лівому куті (помилка, якщо fon == None)
-    window.
+    window.blit(fon, (fon_x,fon_y))
+    window.blit(fon, (fon_x-HEIGHT,fon_y))
+    window.blit(fon, (fon_x,fon_y-WIDTH))
+    window.blit(fon, (fon_x-HEIGHT,fon_y-WIDTH))
+
+    player.draw(window)
+    player.move()
+    fon_x+=player.move_x
+    fon_y+=player.move_y
+    fon_x%=WIDTH
+    fon_y%=HEIGHT
 
     # Перебираємо всі події (натискання клавіш, закриття вікна тощо)
-    for 
+    for event in pygame.event.get():
 
         # Перевіряємо, чи користувач закрив вікно
-        if 
+        if event.type == pygame.QUIT:
 
             # Зупиняємо гру
-            run 
+            run = False
 
     # Оновлюємо зображення на екрані
-    pygame.
+    pygame.display.flip()
 
     # Обмежуємо швидкість гри до 60 кадрів на секунду
-    clock.
+    clock.tick(60)
--- my_class.py
+++ my_class.py
@@ -36,77 +36,22 @@ class Circle(Sprite):
         self.color = color
 
     def draw(self, surface):
-        pygame.draw.rect(surface, (0,0,0), self.rect, width=5)
+        #pygame.draw.rect(surface, (0,0,0), self.rect, width=5)
         pygame.draw.circle(surface, self.color, self.rect.center, self.radius)
+
 class Player(Circle):
-    def __init__(self, x=100, y=100, radius=50, speed=0, color=(0, 0, 130)):
+     def __init__(self, x=100, y=100, radius=50, speed=0, color=(0, 0, 130)):
           super().__init__(x, y, radius, speed, color)
-          self.dx =0
-          self.dy = 0
-    def move(self):
-          self.dx = 0
-          self.dy = 0
-          key = pygame.key.get_pressed()
+          self.move_x = 0 
+          self.move_y = 0
+     def move(self):
+          key = pygame.get_pressed()
           if key[pygame.K_w]:
-               self.dy = self.speed
+               self.move_y = self.speed
           if key[pygame.K_s]:
-               self.dy = -self.speed
+               self.move_y = -self.speed
           if key[pygame.K_a]:
-               self.dx = self.speed
-          if key[pygame.K_d]:
-               self.dx = -self.speed
-         
-         
-class Eat(Circle):
-     def check_collide(self, player):
-          if self.rect.colliderect(player.rect):
-               player.radius += self.radius // 5
-               player.rect.w, player.rect.h = player.radius * 2, player.radius * 2
-               return True
-          return False
+               self.move_x = self.speed 
+          if key[pygame.K_d]: 
+               self.move_x = -self.speed
 
-
-class AnimeSprite(Sprite):
-    def __init__(self, x=10, y=10, w=50, h=50, images=None):
-        super().__init__(x, y, w, h)
-        self.current_frame = 0
-        self.frames = []
-        for image in images:
-             self.image = image
-             self.load_img()
-             self.frames.append(self.image)
-
-   
-
-    def anime(self):
-        self.current_frame += 0.1
-        if self.current_frame >= len(self.frames):
-            self.current_frame = 0
-        self.image = self.frames[int(self.current_frame)]
-
-class Button:
-    def __init__(self, x = 50, y = 50, w = 150, h = 80, 
-                  text="Button", color=(255, 255, 255), 
-                  color_text=(0,0,0), function=None):
-        self.color = color
-        self.color_text = color_text
-        self.rect = pygame.Rect(x, y ,w, h)
-        self.function = function
-        self.font = pygame.font.Font(None, h)
-        self.text = self.font.render(text,True, self.color_text)
-
-    
-    def draw(self, window):
-        pygame.draw.rect(window, self.color,self.rect)
-        pygame.draw.rect(window, self.color_text,self.rect, width=10)
-        x,y  = self.text.get_width(), self.text.get_height()
-        x,y = (self.rect.w - x)//2, (self.rect.h - y)//2
-        x,y = self.rect.x +x, self.rect.y + y
-        window.blit(self.text,(x,y))
-
-    def click(self):
-        be_click = pygame.mouse.get_pressed()[0]
-        pos = pygame.mouse.get_pos()
-        if be_click:
-             if self.rect.collidepoint(pos):
-                  self.function()
