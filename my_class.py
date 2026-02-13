c4b3020d682ce6751a1c5f9bd1030b4de0497ab7
Maria Panteleichuk <varvarich65@gmail.com>
2026-02-13 14:48:38 +0200

    launcher plan

--- my_class.py
+++ my_class.py
@@ -63,6 +63,12 @@ class Player(Circle):
            super().draw(window)
            window.blit(self.nick,(self.rect.x, self.rect.y-8))
 
+      def grow(self, radius):
+          self.radius += radius
+          self.rect.w = self.radius*2
+          self.rect.h = self.radius*2
+          self.rect.center = (self.rect.x + self.rect.w//2, self.rect.y + self.rect.h//2)
+
 class Food(Circle):
      def __init__(self):
           x = randint(-5000,500)
@@ -71,18 +77,14 @@ class Food(Circle):
           speed = 0
           color = (randint(0,255),randint(0,255),randint(0,255))
           super().__init__(x, y, radius, speed, color)
+
      def update(self,player):
           self.rect.x -= player.move_x
           self.rect.y -= player.move_y
-         # self.rect.x %= 500
-         # self.rect.y %= 500
+
      def eat_me(self,player):
          if self.rect.colliderect(player.rect):
-              x,y = player.rect.center
-              player.radius += self.radius
-              player.rect.w = player.radius*2
-              player.rect.h = player.radius*2
-              player.rect.center = (x,y)
+              player.grow(self.radius)
               return True
          else:
               return False
