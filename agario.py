10090db86c93b5834bd79ccf0e3351e5a1f60b74
a6086394-droid <a6086394@gmail.com>
2026-02-14 11:32:09 +0200

    Update agario.py

--- agario.py
+++ agario.py
@@ -10,10 +10,13 @@ from my_class import*  # Класи Player, Food
 '''1. Імпортуємо модулі для мережі і потоків'''
 from socket import socket, AF_INET,SOCK_STREAM  # socket для роботи з сервером
 from threading import Thread # Thread для паралельного отримання даних
-
+from launcher import Launcher # Імпорт класу Launcher для стартового вікна гри
 ''''''''''''
-
-# Ініціалізуємо всі модулі Pygame
+menu = Launcher()  # Створюємо об'єкт стартового вікна
+menu.window_start()
+nick = menu.nick
+ip = menu.ip # Ініціалізуємо всі модулі Pygame
+port = menu.port
 pygame.init()
 
 # Задаємо ширину і висоту вікна гри
@@ -37,7 +40,7 @@ fon = pygame.transform.scale(fon,(WIDTH,HEIGHT))
 
 # Гравець
 player = Player(x=WIDTH//2,y=HEIGHT//2,radius=20,
-                speed=5,color=((randint(0,255)),randint(0,255),randint(0,255)),nickname="ola-la")
+                speed=5,color=((randint(0,255)),randint(0,255),randint(0,255)),nickname=nick)  # Створюємо гравця з випадковим кольором та нікнеймом зі стартового вікна")
 
 # Їжа
 foods = [Food() for i in range(300)]
@@ -51,7 +54,6 @@ fon_x,fon_y = 0,0
 word_x, word_y = 0,0
 other_player = {}  #id:{x:x, y:y, r:r, c:c}
 buffer = ""
-nickname = "ola-la"
 
 '''3. Налаштовуємо сокет клієнта'''
 ''''''
